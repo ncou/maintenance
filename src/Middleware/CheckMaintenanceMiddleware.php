566736a506d981311e88c9e9496ca5d890fb4c08
ncou <nicolas_courade@yahoo.fr>
2020-08-01 15:13:02 +0200

    update

--- src/Middleware/CheckMaintenanceMiddleware.php
+++ src/Middleware/CheckMaintenanceMiddleware.php
@@ -11,7 +11,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use Chiron\MaintenanceMode;
+use Chiron\Maintenance\MaintenanceMode;
 
 class CheckMaintenanceMiddleware implements MiddlewareInterface
 {
